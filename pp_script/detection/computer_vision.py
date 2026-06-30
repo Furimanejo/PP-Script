fc4f68355de90bd9127f77e40cea150fb95ab2f4
Furimanejo <furimanejo@gmail.com>
2026-06-30 15:00:17 -0300

    Clean code

--- pp_script/detection/computer_vision.py
+++ pp_script/detection/computer_vision.py
@@ -7,6 +7,7 @@ import os
 import shutil
 from math import ceil
 from time import perf_counter
+from typing import Callable
 
 from pp_script.core import _logger, Rect, read_file_at_folder_or_zip
 
@@ -28,7 +29,7 @@ class ComputerVision:
         self._debug_folder = debug_path
 
         cv_values = copy.deepcopy(cv_values)
-        _scaling_method = cv_values.get("scaling_method", (1920, 1080))
+        _scaling_method = cv_values.pop("scaling_method", (1920, 1080))
         if isinstance(_scaling_method, tuple):
             ref_rw, ref_rh = _scaling_method
 
@@ -48,7 +49,7 @@ class ComputerVision:
             _scaling_method = scale_from_resolution
 
         self._regions: dict[str, Region] = {}
-        regions: dict = cv_values.get("regions", {})
+        regions: dict = cv_values.pop("regions", {})
         for name, values in regions.items():
             if not isinstance(values, dict):
                 raise ValueError()
@@ -56,12 +57,15 @@ class ComputerVision:
             self._regions[name] = Region(values)
 
         self._templates: dict[str, Template] = {}
-        templates: dict = cv_values.get("templates", {})
+        templates: dict = cv_values.pop("templates", {})
         for name, values in templates.items():
             if not isinstance(values, dict):
                 raise ValueError()
             values.setdefault("scaling_method", _scaling_method)
             self._templates[name] = Template(values, folder_path=load_path)
+
+        for key in cv_values:
+            _logger.warning(f"CV key unused or unexpected: {key}")
 
     def update(self, rect: Rect, enable: bool):
         self._capture = None  # type: ignore
@@ -90,17 +94,16 @@ class ComputerVision:
 
     def capture(
         self,
-        regions: tuple[str] = (),
-        file: str = None,
+        regions: set[str] | tuple[str, ...] = set(),
+        file: str = None,  # type: ignore
         debug=False,
     ):
-        regions = sorted(regions)
-
+        regions = set(sorted(regions))
         capture_rect = self._rect
         offsets = (0, 0)
-
         img = None
-        if file is not None:
+
+        if file:
             path = os.path.join(self._load_path, file)
             img = cv.imread(filename=path)[:, :, ::-1]
             width = img.shape[1]
@@ -203,6 +206,8 @@ class ComputerVision:
         if debug:
             filter_name = filter.__name__ if filter else ""
             self._save_image(template_img, f"{template_name} template {filter_name}")
+            self._save_image(template_mask, f"{template_name} mask")
+
         if t := template_img.dtype != np.uint8:
             raise Exception(f"Unexpected image type {t}")
 
@@ -215,10 +220,31 @@ class ComputerVision:
             )
 
         match_results = cv.matchTemplate(
-            region_img, template_img, cv.TM_SQDIFF, mask=template_mask
+            region_img,
+            template_img,
+            cv.TM_SQDIFF,
+            mask=template_mask,
         )
         min_val, max_val, min_loc, max_loc = cv.minMaxLoc(match_results)
-        confidence = 1 - min_val / template_obj.size
+
+        if len(template_img.shape) == 2:
+            channels = 1
+        else:
+            channels = template_img.shape[2]
+
+        if template_img.dtype == np.uint8:
+            max_channel_val = 255
+        else:
+            raise Exception(f"Unexpected template dtype: {template_img.dtype}")
+
+        # DEPRECATE LATER
+        max_possible = (
+            template_obj.mask_pixel_count * template_obj._base_image.shape[2] * 255**2
+        )
+        # max_possible = template_obj.mask_pixel_count * channels * max_channel_val**2
+
+        confidence = 1 - min_val / max_possible
+
         result = {
             "success": confidence >= template_obj.threshold,
             "region": region_name,
@@ -254,7 +280,12 @@ class ComputerVision:
 
 
 class Capture:
-    def __init__(self, rect: Rect = None, offsets=(0, 0), img=None):
+    def __init__(
+        self,
+        rect: Rect,
+        offsets=(0, 0),
+        img=None,
+    ):
         self._crops = {}
         self._offsets = offsets
         left, top, right, bottom = rect.as_bbox()
@@ -297,10 +328,13 @@ class Capture:
 
 class Region:
     def __init__(self, values: dict):
-        self._original_rect = Rect(values["rect"])
-        self._label_position = values.get("label_position")
-        self._scaling_method = values["scaling_method"]
-        self._scaled_rect = None
+        self._original_rect = Rect(values.pop("rect"))
+        self._label_position = values.pop("label_position", None)
+        self._scaling_method = values.pop("scaling_method")
+        self._scaled_rect: Rect
+
+        for key in values:
+            _logger.warning(f"Region key unused or unexpected: {key}")
 
     @property
     def rect(self):
@@ -316,21 +350,24 @@ class Region:
 
 class Template:
     def __init__(self, values: dict, folder_path: str):
-        self.threshold = values["threshold"]
-        self._scaling_method = values["scaling_method"]
-        self._base_image = None
+        self._base_image: cv.typing.MatLike
+        self._scaled_and_filtered: dict[None | Callable, cv.typing.MatLike] = {}
+        self._scaled_mask: cv.typing.MatLike
+        self.threshold = values.pop("threshold")
+        self._scaling_method = values.pop("scaling_method")
 
-        f = read_file_at_folder_or_zip(folder_path, values["file"])
+        f = read_file_at_folder_or_zip(folder_path, values.pop("file"))
         img_array = np.frombuffer(f, dtype=np.uint8)
         self._base_image = cv.imdecode(img_array, cv.IMREAD_COLOR_RGB)
-        if mask_color := values.get("mask_color", None):
+
+        if mask_color := values.pop("mask_color", None):
             mask = cv.inRange(self._base_image, mask_color, mask_color)
             self._base_mask = cv.bitwise_not(mask)
         else:
-            self._base_mask = cv.inRange(self._base_image, (0, 0, 0), (255, 255, 255))
+            self._base_mask = cv.inRange(self._base_image, (0, 0, 0), (255, 255, 255))  # type: ignore
 
-        self._scaled_mask = None
-        self._scaled_and_filtered = {}
+        for key in values:
+            _logger.warning(f"Template key unused or unexpected: {key}")
 
     def scale(self, rect: Rect):
         rx, ry, rw, rh = rect.as_tuple()
@@ -344,7 +381,7 @@ class Template:
         mask = self._base_mask
         if scaled_w != w or scaled_h != h:
             template = cv.resize(
-                self._base_image,
+                self._base_image,  # type: ignore
                 (scaled_w, scaled_h),
                 interpolation=cv.INTER_NEAREST_EXACT,
             )
@@ -354,12 +391,12 @@ class Template:
                 interpolation=cv.INTER_NEAREST_EXACT,
             )
 
-        self.size = cv.countNonZero(mask) * template.shape[2] * 255 * 255
         self._scaled_and_filtered = {None: template}
         self._scaled_mask = mask
+        self.mask_pixel_count = cv.countNonZero(self._scaled_mask)
 
-    def scaled_and_filtered(self, filter: callable = None):
-        if filter not in self._scaled_and_filtered:
+    def scaled_and_filtered(self, filter: Callable | None = None):
+        if filter and filter not in self._scaled_and_filtered:
             not_filtered = self._scaled_and_filtered[None]
             self._scaled_and_filtered[filter] = filter(not_filtered)
         return self._scaled_and_filtered[filter], self._scaled_mask
@@ -370,11 +407,11 @@ def cv_in_range(img, lower: tuple, upper: tuple):
         # For HSV filters around the red hue (H=0) the H value can wrap around,
         # for example, if lower=(200, 10, 10) and upper=(30, 50, 50) then we do
         # range (0, 10, 10)(30, 50, 50) + range (200, 10, 10)(255, 50, 50)
-        first = cv.inRange(img, (0,) + lower[1:], upper)
-        second = cv.inRange(img, lower, (255,) + upper[1:])
+        first = cv.inRange(img, (0,) + lower[1:], upper)  # type: ignore
+        second = cv.inRange(img, lower, (255,) + upper[1:])  # type: ignore
         return cv.bitwise_or(first, second)
     else:
-        return cv.inRange(img, lower, upper)
+        return cv.inRange(img, lower, upper)  # type: ignore
 
 
 def cv_to_hsv(img):
